8e991917d0a5aef754d6e4ea409c8bfa4bcc5cf2
a-a-robbins <a.a.robbins@outlook.com>
2022-10-07 11:25:07 -0500

    via Trong: add timing, fix overflow on quicksort

--- Quicksort.java
+++ Quicksort.java
@@ -14,18 +14,10 @@ class Quicksort {
    
       for(int i = 0; i < arr.length; i++) {
       
-         arr[i] = rand.nextInt(100); //DON'T FORGET TO CHANGE ME!!
+         arr[i] = rand.nextInt(); 
          
       }
    
-   }
-   
-   private void swap(int i, int j) {
-   
-      int temp = i;
-      i = j;
-      j = temp;
-      
    }
    
    public static void quicksort(int[] arr, int cutoff) {
@@ -35,92 +27,82 @@ class Quicksort {
    }
 
    private static void quicksort(int arr[], int low, int high, int cutoff) {
-   		
-            if (arr.length <= cutoff) {
-   				insertionSort(arr, low, high);
-   			}
+         
+            if (((high-low) <= cutoff) && ((high-low) > 0)) {
+               insertionSort(arr, low, high);
+               
+            }
             
-   			else {
-   				int pivot = partition(arr, low, high);
-   
-   					quicksort(arr, low, pivot - 1, cutoff);
-   				
-   					quicksort(arr, pivot + 1, high, cutoff);
-   				
-   			}
-   		}
-   	
+            else {
+               if(low < high){
+                  int pivot = partition(arr, low, high);
+                  quicksort(arr, low, pivot - 1, cutoff);
+                  quicksort(arr, pivot + 1, high, cutoff);
+               }
+               
+               
+            }
+         }
+      
    
 
-	private static int partition(int arr[], int low, int high) {
+   private static int partition(int arr[], int low, int high) {
    
-		int pivot = arr[high];
-		int i = low-1;
-		int j = low;
-
-		while (i <= high) {
-			if (arr[i] > pivot)
-				i++;
-			else {
-				int temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
-            //FIXME: Trong, can you figure out why this doesn't work? I copied it from lecture code...
-            //swap(arr[i], arr[j]);
-				i++;
-				j++;
-			}
-		}
-		return i + 1;
-	}
+      int pivot = arr[high];
+      int i = low-1;
+      
+      for (int j = low; j <= high -1; j++){
+         if(arr[j] <= pivot){
+            i = i + 1;
+            //swap arr[i] with arr[j]
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+         }
+      }
+      int temp = arr[i+1];
+      arr[i+1] = arr[high];
+      arr[high] = temp;
+      return i + 1;
+   }
    
-   	private static void insertionSort(int arr[], int left, int right) {
+      private static void insertionSort(int arr[], int left, int right) {
    
-		for (int i = left + 1; i <= right; i++) {
+      for (int i = left + 1; i <= right; i++) {
       
          int key = arr[i]; 
          int j = i - 1; 
-	
+   
             while(j >= 0 && arr[j] > key) {
             
                arr[j + 1] = arr[j]; 
                j = j - 1; 
                
-				}
+            }
             
             arr[j + 1] = key; 
-			}
-	}
+         }
+   }
 
-		public static void main(String[] args) {
+      public static void main(String[] args) {
 
-		int size = 9; //this will go in the for loop like our last program
-      int cutoff = 10; 
-      
+      int size = 8000000;
       int[] arr = new int[size]; 
       
-      generateArray(arr); 
-
-      //TEST//
-      System.out.print("generated array arr: "); 
-      for(int i = 0; i < arr.length-1; i++) {
-         System.out.print(arr[i] + ", "); 
+       
+      for(int cutoff = 0; cutoff <= 200; cutoff += 20){
+         int totalTime = 0;
+         for(int i = 1; i <= 20; i++){
+            long time = 0;
+            generateArray(arr);
+            long start = System.currentTimeMillis();
+            quicksort(arr, cutoff);
+            time += System.currentTimeMillis() - start;
+            totalTime += time;
+            System.out.println("time:" + time);
+         }
+         int averageTime = totalTime/20;
+         System.out.println("Cutoff: " + cutoff  + "\t Time: " + averageTime);
       }
-      System.out.println(arr[arr.length-1]); 
-      //END//
-
-		quicksort(arr, cutoff);
-      
-      //TEST
-      System.out.print("sorted array arr: "); 
-      for(int i = 0; i < arr.length-1; i++) {
-         System.out.print(arr[i] + ", "); 
-      }
-      System.out.println(arr[arr.length-1]); 
-      //END//
-
-	}
+   }
 }
-
-
-
